02c50b75ac9a6beb3b9a0e9e566941c3ac77e1ef
Emon Adi <me@emonadi.com>
2024-03-18 16:28:06 +0600

    Basic things done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+    <section className="padding">Popular product</section>
+    <section className="padding">Super Quantity</section>
+    <section className="padding-x py-10">Services</section>
+    <section className="padding">Special Offer</section>
+    <section className="padding bg-pale-blue">Customer reviews</section>
+    <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+    <section className="padding bg-black pb-8 padding-t padding-x">
+      Footer
+    </section>
+  </main>
+);
+
+export default App;
